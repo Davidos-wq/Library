43fc65d772fe295d629dd5facec458dab189a277
Davidos-wq <davidaliev2017@gmail.com>
2026-07-20 16:08:17 +0300

    Final_registrationV1

--- project_run/registration.py
+++ project_run/registration.py
@@ -5,6 +5,9 @@ from aiogram.fsm.state import State,StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import CommandStart
 from registrationbuttons import get_my_keyboard
+from crud import add_info
+from database import async_session_factroy
+from aiogram import F
 
 form_router = Router()
 
@@ -28,12 +31,29 @@ async def process_lastname(message:Message,state:FSMContext):
     await state.update_data(lastname=message.text)
     await message.answer("Виберіть ваший улюблений жанр із запропонованих",reply_markup=get_my_keyboard())
 
-@form_router.callback_query()
+@form_router.callback_query(F.data.in_(["Science Fiction","Fantasy",
+                                        "Mystery","Romance","Horror",
+                                        "Literary Fiction"]))
 async def process_genre(callback:CallbackQuery,state:FSMContext):
-    genre = callback.data
+  
     all_data = await state.get_data() # Всі тимчасові збережені зміни
+    user_id = callback.from_user.id
 
+    name = all_data.get("name")
+    lastname = all_data.get("lastname")
+    genre = callback.data
+    
+    async with async_session_factroy() as session:
+        await add_info(session,user_id,
+                       name,lastname,genre)
+    
+    await callback.message.answer("Ви були зареєстровані")
+    
 
+        
+    
+
+    
 
     
     

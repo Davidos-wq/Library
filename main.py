60782de9b1dc42e7dbda16a4079a94b707e09caf
Davidos-wq <davidaliev2017@gmail.com>
2026-07-20 16:24:51 +0300

    Initmodel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from aiogram import Bot,Dispatcher
 from project_run.registration import form_router
+from models import init_models
 
 from os import getenv
 
@@ -9,6 +10,8 @@ TOKEN = getenv("BOT_TOKEN")
 dp = Dispatcher()
 
 async def main() -> None:
+    await init_models()
+    
     dp.include_router(form_router)
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
